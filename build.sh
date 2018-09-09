a73eb91b843ce571c312481015fbc41d1bcfe1d6
Terren Peterson <terrenjpeterson@yahoo.com>
2018-09-09 17:10:01 +0000

    added automation of a single unit test case invoking the lambda function to verify at the end of the build pipelline

--- build.sh
+++ build.sh
@@ -14,9 +14,23 @@ aws s3 cp lambda.js s3://hurricane-data/binary/
 rm hurricane.zip
 
 # set which lambda function is being updated
-lambdaruntime='hurricaneSkillBlue'
+lambdaruntime='hurricaneSkillGreen'
+#lambdaruntime='hurricaneSkillBlue'
 echo 'deploying new function to' $lambdaruntime
 
 # update the lambda function with the binaries that have been staged
 aws lambda update-function-code --function-name "$lambdaruntime" --s3-bucket hurricane-data --s3-key binary/hurricane.zip >> temp.log
 
+# read in test data required for a request to simulate launching the skill
+echo 'test case 1 - launch request'
+cd testdata
+request=$(<request.json)
+cd ..
+
+# invoke the new lambda function
+aws lambda invoke --function-name "$lambdaruntime" --payload "$request" testOutput.json
+
+# read response file into local variable then print on the console
+response=$(<testOutput.json)
+echo $response
+echo 'test case 1 complete'
